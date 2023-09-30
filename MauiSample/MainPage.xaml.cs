bd41a73dde7f7291e3af7de4c359bfdc1fe6812c
Ashok Kuvaraja <kuvarajaashok@gmail.com>
2023-09-30 20:07:26 +0530

    Initial commit

---
+++ MauiSample/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel
+    {
+        public List<string> items = new();
+
+        public ViewModel()
+        {
+            GenerateItems();
+        }
+
+        public List<string> Items
+        {
+            get => items;
+        }
+
+        void GenerateItems()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Items.Add("Label " + i.ToString());
+            }
+        }
+    }
+}
+
+
